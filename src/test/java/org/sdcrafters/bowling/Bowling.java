90672561bba0896c5a5ff3d4dea3d17038998055
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:52:36 -0700

    !!! starting duplication

--- src/test/java/org/sdcrafters/bowling/Bowling.java
+++ src/test/java/org/sdcrafters/bowling/Bowling.java
@@ -7,14 +7,20 @@ public class Bowling {
 	ArrayList<Frame> frames = new ArrayList<>();
 
 	public void roll(Integer... rolls) {
-		Frame frame = new Frame(1, 0);
-		frame.addRoll(rolls[0]);
-		frame.addRoll(rolls[1]);
-		frames.add(frame);
-		frame = new Frame(2, frame.getTotalScore());
-		frame.addRoll(rolls[2]);
-		frame.addRoll(rolls[3]);
-		frames.add(frame);
+		int previousScore = 0;
+		{
+			Frame frame = new Frame(1, 0);
+			frame.addRoll(rolls[0]);
+			frame.addRoll(rolls[1]);
+			frames.add(frame);
+			previousScore = frame.getTotalScore();
+		}
+		{
+			Frame frame = new Frame(2, previousScore);
+			frame.addRoll(rolls[2]);
+			frame.addRoll(rolls[3]);
+			frames.add(frame);
+		}
 	}
 
 	@Override
