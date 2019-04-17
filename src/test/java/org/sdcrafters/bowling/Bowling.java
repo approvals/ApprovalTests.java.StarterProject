304626a49754351b958aed6ba78c3614c21d78fd
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:30:02 -0700

    t delete comments

--- src/test/java/org/sdcrafters/bowling/Bowling.java
+++ src/test/java/org/sdcrafters/bowling/Bowling.java
@@ -3,13 +3,11 @@ package org.sdcrafters.bowling;
 public class Bowling {
 
 	public void roll(Integer... rolls) {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
 		return "1) 1,1 [2] = 2";
 	}
 
