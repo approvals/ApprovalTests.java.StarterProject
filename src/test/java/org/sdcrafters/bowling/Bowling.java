4e0c26dd0ca418fd2f7374ab981b79b0f26d6a5c
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:54:07 -0700

    !!! rollIndex

--- src/test/java/org/sdcrafters/bowling/Bowling.java
+++ src/test/java/org/sdcrafters/bowling/Bowling.java
@@ -7,12 +7,13 @@ public class Bowling {
 	ArrayList<Frame> frames = new ArrayList<>();
 
 	public void roll(Integer... rolls) {
-		int previousScore = 0;
+		int rollIndex = 0;
+		int previousScore = rollIndex;
 		{
 			int frameNumber = 1;
 			Frame frame = new Frame(frameNumber, previousScore);
-			frame.addRoll(rolls[0]);
-			frame.addRoll(rolls[1]);
+			frame.addRoll(rolls[rollIndex++]);
+			frame.addRoll(rolls[rollIndex]);
 			frames.add(frame);
 			previousScore = frame.getTotalScore();
 		}
