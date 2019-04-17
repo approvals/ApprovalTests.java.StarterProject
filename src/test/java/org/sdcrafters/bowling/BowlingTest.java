881478b302df2fb6e42b8dc059beeae680bf920b
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:30:29 -0700

    t cleaning

--- src/test/java/org/sdcrafters/bowling/BowlingTest.java
+++ src/test/java/org/sdcrafters/bowling/BowlingTest.java
@@ -7,11 +7,8 @@ public class BowlingTest {
 
 	@Test
 	public void test() {
-		// create a bowl game
 		Bowling b = new Bowling();
-		// roll 1,1,2,2,3,3,4,4,5,5,3,2,10,3,2,10,10,10,10
 		b.roll(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 3, 2, 10, 3, 2, 10, 10, 10, 10);
-		// verify score
 		Approvals.verify(b.toString());
 	}
 
