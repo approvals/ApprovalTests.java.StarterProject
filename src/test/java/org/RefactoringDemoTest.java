14913f5fa8642c9d40fcaf4abeedcca9ca5b0b7b
Llewellyn Falco <isidore@setgame.com>
2019-04-16 22:42:36 -0700

    inline

--- src/test/java/org/RefactoringDemoTest.java
+++ src/test/java/org/RefactoringDemoTest.java
@@ -37,11 +37,10 @@ public class RefactoringDemoTest {
 			double sqrt_5 = 2.2360679775;
 			double Phi = (1.0 + sqrt_5) / 2.0;
 			double phi = (1.0 - sqrt_5) / 2.0;
-			int value1 = n - 1;
-			int fib1 = (int) Math.round((Math.pow(Phi, value1) - Math.pow(phi, value1)) / sqrt_5);
+			int fib1 = (int) Math.round((Math.pow(Phi, n - 1) - Math.pow(phi, n - 1)) / sqrt_5);
 			int fib2 = (int) Math.round((Math.pow(Phi, n - 2) - Math.pow(phi, n - 2)) / sqrt_5);
 
-			return fibonacci_direct(value1) + fibonacci_direct(n - 2);
+			return fibonacci_direct(n - 1) + fibonacci_direct(n - 2);
 		}
 
 	}
