ec71476b552cdfc421e7feb9a5aba0de8af91ec6
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:44:44 -0700

    !!! many frames

--- src/test/java/org/sdcrafters/bowling/Bowling.java
+++ src/test/java/org/sdcrafters/bowling/Bowling.java
@@ -1,17 +1,21 @@
 package org.sdcrafters.bowling;
 
+import java.util.ArrayList;
+
 public class Bowling {
 
-	Frame frame = new Frame();
+	ArrayList<Frame> frames = new ArrayList<>();
 
 	public void roll(Integer... rolls) {
+		Frame frame = new Frame();
 		frame.addRoll(rolls[0]);
 		frame.addRoll(rolls[1]);
+		frames.add(frame);
 	}
 
 	@Override
 	public String toString() {
-		return frame.toString();
+		return frames.get(0).toString();
 	}
 
 }
