faeb0530db469011c4974afa00b55d7b8fb537d8
Llewellyn Falco <isidore@setgame.com>
2019-04-16 22:38:38 -0700

    fib1+fib2

--- src/test/java/org/RefactoringDemoTest.java
+++ src/test/java/org/RefactoringDemoTest.java
@@ -37,12 +37,12 @@ public class RefactoringDemoTest {
 			double sqrt_5 = 2.2360679775;
 			double Phi = (1.0 + sqrt_5) / 2.0;
 			double phi = (1.0 - sqrt_5) / 2.0;
-			int value1 = n;
+			int value1 = n - 1;
 			int fib1 = (int) Math.round((Math.pow(Phi, value1) - Math.pow(phi, value1)) / sqrt_5);
-			int value2 = n;
+			int value2 = n - 2;
 			int fib2 = (int) Math.round((Math.pow(Phi, value2) - Math.pow(phi, value2)) / sqrt_5);
 
-			return fib1;
+			return fib1 + fib2;
 		}
 
 	}
