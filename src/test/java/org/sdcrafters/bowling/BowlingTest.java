1c1b46e35908d492359633718f64c3ad628123d5
Llewellyn Falco <isidore@setgame.com>
2019-04-16 19:27:20 -0700

    *** code

--- src/test/java/org/sdcrafters/bowling/BowlingTest.java
+++ src/test/java/org/sdcrafters/bowling/BowlingTest.java
@@ -1,14 +1,18 @@
 package org.sdcrafters.bowling;
 
-import static org.junit.Assert.*;
-
+import org.approvaltests.Approvals;
 import org.junit.Test;
 
 public class BowlingTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		// create a bowl game
+		Bowling b = new Bowling();
+		// roll 1,1,2,2,3,3,4,4,5,5,3,2,10,3,2,10,10,10,10
+		b.roll(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 3, 2, 10, 3, 2, 10, 10, 10, 10);
+		// verify score
+		Approvals.verify(b);
 	}
 
 }
