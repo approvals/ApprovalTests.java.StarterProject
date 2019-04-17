bb33df24ce91e94d65a076d4525dc33304acfe5d
Llewellyn Falco <isidore@setgame.com>
2019-04-16 22:31:38 -0700

    inline

--- src/test/java/org/RefactoringDemoTest.java
+++ src/test/java/org/RefactoringDemoTest.java
@@ -33,9 +33,7 @@ public class RefactoringDemoTest {
 			return 0;
 		} else if (n == 1) {
 			double sqrt_5 = 2.2360679775;
-			double phi = (1.0 - sqrt_5) / 2.0;
-
-			int fib = (int) Math.round(((1.0 + sqrt_5) / 2.0 - phi) / sqrt_5);
+			int fib = (int) Math.round(((1.0 + sqrt_5) / 2.0 - ((1.0 - sqrt_5) / 2.0)) / sqrt_5);
 			return fib;
 		} else {
 			double sqrt_5 = 2.2360679775;
