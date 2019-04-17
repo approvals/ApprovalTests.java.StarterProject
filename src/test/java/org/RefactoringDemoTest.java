51fbee9d1f160889c3a29ae2e069cdf5f943c828
Llewellyn Falco <isidore@setgame.com>
2019-04-16 22:38:18 -0700

    introduce value2

--- src/test/java/org/RefactoringDemoTest.java
+++ src/test/java/org/RefactoringDemoTest.java
@@ -39,7 +39,9 @@ public class RefactoringDemoTest {
 			double phi = (1.0 - sqrt_5) / 2.0;
 			int value1 = n;
 			int fib1 = (int) Math.round((Math.pow(Phi, value1) - Math.pow(phi, value1)) / sqrt_5);
-			int fib2 = (int) Math.round((Math.pow(Phi, value1) - Math.pow(phi, value1)) / sqrt_5);
+			int value2 = n;
+			int fib2 = (int) Math.round((Math.pow(Phi, value2) - Math.pow(phi, value2)) / sqrt_5);
+
 			return fib1;
 		}
 
