942590df1638c227be67f94daa66d4c6f29114b2
Llewellyn Falco <isidore@setgame.com>
2019-05-07 15:07:13 +0200

    "automate buzz"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -7,7 +7,7 @@ public class FizzBuzz
     String delimiter = ",";
     String result = "";
     String fizz = "Fizz";
-    for (int j = 1; j <= 4; j++)
+    for (int j = 1; j <= 5; j++)
     {
       if (j == 3)
       {
@@ -22,6 +22,6 @@ public class FizzBuzz
         result += j + delimiter;
       }
     }
-    return result + "Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return result + "Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
