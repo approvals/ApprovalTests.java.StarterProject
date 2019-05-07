2d85dd29c6ed6d46b25014cb4239165958030875
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:57:48 +0200

    "fizz encapsulated"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -10,6 +10,7 @@ public class FizzBuzz
     {
       result += j + delimiter;
     }
-    return result + "Fizz" + ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    String fizz = "Fizz";
+    return result + fizz + ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
