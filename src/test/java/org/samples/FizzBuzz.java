bc9ef37b7dc290e759aa3ad36af1f98922538a0a
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:28:12 +0200

    "number sliced"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz
 {
   public static String call(int i)
   {
-    return "1" + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    String number = "1";
+    return number + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
