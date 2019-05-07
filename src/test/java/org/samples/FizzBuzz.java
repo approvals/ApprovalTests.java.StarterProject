d85e35927e6c544fb1a8f75812b1767ab011624b
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:25:21 +0200

    "one separated"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -4,6 +4,6 @@ public class FizzBuzz
 {
   public static String call(int i)
   {
-    return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return "1" + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
