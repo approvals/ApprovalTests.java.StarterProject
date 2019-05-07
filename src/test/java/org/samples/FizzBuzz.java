ab8c904aef23c156e29d2303165a6ac0cdbc4b31
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:55:26 +0200

    "simplified loop"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -7,10 +7,9 @@ public class FizzBuzz
     String delimiter = ",";
     String number2 = "" + 2;
     String result = "";
-    for (int j = 1; j <= 1; j++)
+    for (int j = 1; j <= 2; j++)
     {
       result += j + delimiter;
-      result += number2 + delimiter;
     }
     return result + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
