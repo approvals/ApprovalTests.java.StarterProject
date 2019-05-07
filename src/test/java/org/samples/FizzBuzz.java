dfdaedb725b2b731caa40b46902ae54c4c3ce1bb
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:33:51 +0200

    "encapsulated comma"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -5,6 +5,7 @@ public class FizzBuzz
   public static String call(int i)
   {
     String number = "" + 1;
-    return number + "," + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    String delimiter = ",";
+    return number + delimiter + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
