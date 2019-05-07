290b64d39855160fb1acc98e542cbb9dcc55229a
Llewellyn Falco <isidore@setgame.com>
2019-05-07 15:27:07 +0200

    "module 3 condition added"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -9,7 +9,7 @@ public class FizzBuzz
     String fizz = "Fizz";
     for (int j = 1; j <= 9; j++)
     {
-      if (j == 3 || j == 6 || j == 9)
+      if (j % 3 == 0)
       {
         result += fizz;
       }
