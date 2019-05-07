58ef3d2d0f62e0c26a3c3b5f47b7ca2b1954746c
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:32:30 +0200

    "separated comma"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -5,6 +5,6 @@ public class FizzBuzz
   public static String call(int i)
   {
     String number = "" + 1;
-    return number + ",2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return number + "," + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
