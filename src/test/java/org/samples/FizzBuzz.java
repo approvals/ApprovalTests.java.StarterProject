7b6f93b7e46454db7caabc6b5ac3e0f4472139fd
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:47:56 +0200

    "separating one per line"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -8,7 +8,8 @@ public class FizzBuzz
     String delimiter = ",";
     String number2 = "" + 2;
     String result = "";
-    result += number1 + delimiter + number2 + delimiter;
+    result += number1 + delimiter;
+    result += number2 + delimiter;
     return result + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
