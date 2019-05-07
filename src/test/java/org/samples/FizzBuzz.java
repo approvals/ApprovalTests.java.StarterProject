e30eaec3e9637d5819728c9053ff369760adefcb
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:19:31 +0200

    "Create function"

---
+++ src/test/java/org/samples/FizzBuzz.java
@@ -0,0 +1,9 @@
+package org.samples;
+
+public class FizzBuzz
+{
+  public static String call(int i)
+  {
+    return "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+  }
+}
