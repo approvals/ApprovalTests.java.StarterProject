521ca6508091d522c9da2b636cbb63de993b8a2b
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:57:17 +0200

    "separated fizz"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -10,6 +10,6 @@ public class FizzBuzz
     {
       result += j + delimiter;
     }
-    return result + "Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return result + "Fizz" + ",4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
