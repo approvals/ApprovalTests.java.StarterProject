52a3290e4d651a7080a02b2d8d85ea2c18d98e61
Llewellyn Falco <isidore@setgame.com>
2019-05-07 14:35:04 +0200

    "rename"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -4,8 +4,8 @@ public class FizzBuzz
 {
   public static String call(int i)
   {
-    String number = "" + 1;
+    String number1 = "" + 1;
     String delimiter = ",";
-    return number + delimiter + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return number1 + delimiter + "2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
