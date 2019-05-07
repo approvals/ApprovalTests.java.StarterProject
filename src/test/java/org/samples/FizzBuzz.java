3fbc35b04a90970310159252d2335b746bd8a84b
Llewellyn Falco <isidore@setgame.com>
2019-05-07 15:08:15 +0200

    "automate 2nd fizz"

--- src/test/java/org/samples/FizzBuzz.java
+++ src/test/java/org/samples/FizzBuzz.java
@@ -7,9 +7,9 @@ public class FizzBuzz
     String delimiter = ",";
     String result = "";
     String fizz = "Fizz";
-    for (int j = 1; j <= 5; j++)
+    for (int j = 1; j <= 6; j++)
     {
-      if (j == 3)
+      if (j == 3 || j == 6)
       {
         result += fizz + delimiter;
       }
@@ -22,6 +22,6 @@ public class FizzBuzz
         result += j + delimiter;
       }
     }
-    return result + "Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
+    return result + "7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz";
   }
 }
