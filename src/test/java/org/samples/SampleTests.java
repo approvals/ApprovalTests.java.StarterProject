ccf388088c7523e7d6b4a78fde8eccff631522f9
Llewellyn Falco <isidore@setgame.com>
2019-05-16 09:18:04 +0200

    Added Main folder & verifyAsJson example

--- src/test/java/org/samples/SampleTests.java
+++ src/test/java/org/samples/SampleTests.java
@@ -1,19 +1,30 @@
 package org.samples;
+
 import static org.junit.Assert.assertEquals;
 
 import org.approvaltests.Approvals;
 import org.junit.Test;
 
-public class SampleTests {
-
-	@Test
-	public void testNormalJunit() {
-		assertEquals(5, 5);
-	}
-
-	@Test
-	public void testWithApprovalTests() {
-		Approvals.verify("Hello World");
-	}
-
+public class SampleTests
+{
+  @Test
+  public void testNormalJunit()
+  {
+    assertEquals(5, 5);
+  }
+  @Test
+  public void testWithApprovalTests()
+  {
+    Approvals.verify("Hello World");
+  }
+  /**
+    *  note: this requires GSON which is currently added in the pom.xml file. 
+    *  This is only required if you want to use the VerifyAsJson.
+    **/
+  @Test
+  public void testJson()
+  {
+    Person hero = new Person("jayne", "cobb", true, 38);
+    Approvals.verifyAsJson(hero);
+  }
 }
