e5e79e1a18383f1c7d0fae790834ce838fa12930
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-11-09 10:40:02 -0700

    Update install.windows.ps1

--- install.windows.ps1
+++ install.windows.ps1
@@ -5,24 +5,14 @@
 #
 
 
-#install chocolatey
-iwr -useb cin.st | iex
-Set-Service Audiosrv -StartupType Automatic
-
-#install common tools
-choco install -y vscode win-no-annoy git github-desktop anydesk.install googlechrome
-start-process https://github.com/GreatWebGuy/MobTime/releases
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
+iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/java-intellij.ps1 | iex
 
 #install project specific tools 
-choco install -y notepadplusplus beyondcompare jdk8 intellijidea maven
+choco install -y jdk8 
 
 # Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.java.StarterProject.git C:\Code\ApprovalTests.java.StarterProject
-
-
-#Set desktop background image
- set-itemproperty -path "HKCU:Control Panel\Desktop" -name WallPaper -value c:\Code\TestingLegacyCodeCourse.cpp\Resources\desktop_background_dark.png
- RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
 
 # Done
 cls
